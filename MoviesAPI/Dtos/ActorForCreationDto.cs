3f1d98c7564eddec38621a0c6e84556f94a48313
Oluwaseyi Ademola <oeademola@gmail.com>
2020-05-15 20:53:33 +0100

    Commit changes

--- MoviesAPI/Dtos/ActorForCreationDto.cs
+++
@@ -1,8 +0,0 @@
-namespace MoviesAPI.Dtos
-{
-    public class ActorForCreationDto
-    {
-        public int PersonId { get; set; }
-        public string Character { get; set; }
-    }
-}
--- MoviesAPI/Dtos/HashResult.cs
+++
@@ -1,8 +0,0 @@
-namespace MoviesAPI.Dtos
-{
-    public class HashResult
-    {
-        public string Hash { get; set; }
-        public byte[] Salt { get; set; }
-    }
-}
